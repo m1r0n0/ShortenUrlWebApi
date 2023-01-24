e65fc76c743a7c6761517b2a3ec9449ae214fd37
Олександр <mironuk29@gmail.com>
2023-01-24 18:10:44 +0200

    changed PrivacyChanging

---
+++ DataAccessLayer/Exeptions/BadRequestException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Exeptions
+{
+    public class BadRequestException : Exception
+    {
+        BadRequestException()
+        {
+
+        }
+    }
+}
